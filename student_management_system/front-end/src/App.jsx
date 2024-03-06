9ce5bb13e7cb3928af96d36c9d491e98f0370afa
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-03-06 20:42:15 +0530

    test of form component

--- student_management_system/front-end/src/App.jsx
+++ student_management_system/front-end/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
   return (
@@ -9,6 +7,7 @@ function App() {
       <div className="bg-yellow-600 text-white text-4xl font-mono rounded-full p-4">
         Student Management System
       </div>
+      <Form title="Launch Course" />
     </>
   );
 }
