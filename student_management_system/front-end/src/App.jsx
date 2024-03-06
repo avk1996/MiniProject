a17cb254868486e604dd2863ce1371d1b3b02698
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-03-06 20:25:57 +0530

    front end work started

---
+++ student_management_system/front-end/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-yellow-600 text-white text-4xl font-mono rounded-full p-4">
+        Student Management System
+      </div>
+    </>
+  );
+}
+
+export default App;
