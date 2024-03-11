2b7fe8596fcffa41bd860e6f773aa67d923a1885
Abhishek Kumbhar <62824193+avk1996@users.noreply.github.com>
2024-03-11 22:54:22 +0530

    movie management back-end project init

---
+++ movie_management_system/back-end/spring_boot_backend_template/src/main/java/com/app/MovieApplication.java
@@ -0,0 +1,19 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+// annotation to let know what this class does.
+@SpringBootApplication
+// to let know java that this class is rest controller class
+@RestController
+public class MovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApplication.class, args);
+	}
+	
+	// first end-point
+
+}
